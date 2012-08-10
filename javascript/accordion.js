6a9cbe197018b75a8fe38f8f037b228cef81e960
Radoslav Stankov <rstankov@gmail.com>
2012-08-10 22:25:10 +0300

    Add sample html structure

---
+++ javascript/accordion.js
@@ -0,0 +1,14 @@
+/*
+ Sample html code:
+
+  <ul class="accordion-menu">
+    <li class="item">
+      <a href="#" class="section-1">Section 1</a>
+      <ul id="section-1">
+        <li>...</li> 
+        <li>...</li>
+        <li>...</li>
+      </ul>
+    </li>
+  </ul>
+*/
