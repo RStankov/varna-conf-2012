ed4c50f933e409ed1611e4d65690bc8c06c594d8
Radoslav Stankov <rstankov@gmail.com>
2012-08-10 22:28:05 +0300

    Add the UGLY javascript sample code

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -12,3 +12,22 @@
     </li>
   </ul>
 */
+
+$('.accordion-menu .item a').each(function() {
+  $(this).bind('click', function() {
+    var title = this.className;
+    var theul = $('#' + title);
+    if (theul.length > 0) {
+      if (theul.css('display') == 'none') {
+        theul.slideDown();
+        var theli = dropa.parent('li');
+        theli.animate({'padding-bottom': '20px'});
+      } else {
+        theul.slideUp(); 
+        var theli = dropa.parent('li');
+        theli.animate({'padding-bottom': '20px'});
+      }
+    } else {
+    }
+  });
+});
