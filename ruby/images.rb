bd7af80866cc1c1d80e45db5ae1e671ea76d5a4e
Radoslav Stankov <rstankov@gmail.com>
2012-08-10 23:12:38 +0300

    A little bit more cleaning

--- ruby/images.rb
+++ ruby/images.rb
@@ -4,35 +4,14 @@ class FriendImages
   end
 
   def images(sorting, page)
-    images = find_images_of(friends)
-    images = apply_age_restritions_to(images)
-    images = order_by(images, sorting)
-    images = paginate(images, page)
-
-    decorate(images)
+    decorate Image.of(friends)
+      .of(friends)
+      .appropriate_for(@user.age)
+      .order(sorting)
+      .paginate(page)
   end
 
   private
-
-  def friends
-    Friendship.friend_ids_of(@user)
-  end
-
-  def find_images_of(friends)
-    Image.of(friends)
-  end
-
-  def apply_age_restritions_to(images)
-    images.appropriate_for(@user.age)
-  end
-
-  def order_by(sorting)
-    images.order(sorting)
-  end
-
-  def paginate(page)
-    images.paginate(page)
-  end
 
   def decorate(images)
     images.map { |image| ImageDecorator.new(image) }
