a596d650632c904b3474269d69aa671966d564e6
Radoslav Stankov <rstankov@gmail.com>
2012-08-10 23:02:53 +0300

    Revert stupid change (DRY example)

--- ruby/images.rb
+++ ruby/images.rb
@@ -14,21 +14,3 @@ class NewsestFriendImages
 end
 
 NewsestFriendImages.new(current_user).images(params[:page]) 
-
-class MostLikedFriendImages
-  def initialize(user)
-    @user = user
-  end
-
-  def images(page)
-    friends = Friendship.where(user_id: @user.id).pluck('friend_id')
-    images  = Image.where(user_id: friends, visible_to_friends: true)
-    images  = images.where('minimum_age >= ?', @user.age)
-    images  = images.order('votes_count')
-    images  = images.limit(PER_PAGE).offset(page.to_i.abs * PER_PAGE)
-    images.map { |image| ImageDecorator.new(image) }
-  end
-end
-
-NewsestFriendImages.new(current_user).images(params[:page]) 
-
