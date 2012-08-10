74e28f8fc980af267ba02f0a87fbde3c45f2db46
Radoslav Stankov <rstankov@gmail.com>
2012-08-10 23:09:11 +0300

    Apply Single Level of Abstraction Principle (SLAP)

--- ruby/images.rb
+++ ruby/images.rb
@@ -4,11 +4,37 @@ class FriendImages
   end
 
   def images(sorting, page)
-    friends = Friendship.where(user_id: @user.id).pluck('friend_id')
-    images  = Image.where(user_id: friends, visible_to_friends: true)
-    images  = images.where('minimum_age >= ?', @user.age)
-    images  = images.order(sorting)
-    images  = images.limit(PER_PAGE).offset(page.to_i.abs * PER_PAGE)
+    images = find_images_of(friends)
+    images = apply_age_restritions_to(images)
+    images = order_by(images, sorting)
+    images = paginate(images, page)
+
+    decorate(images)
+  end
+
+  private
+
+  def friends
+    Friendship.where(user_id: @user.id).pluck('friend_id')
+  end
+
+  def find_images_of(friends)
+    Image.where(user_id: friends, visible_to_friends: true)
+  end
+
+  def apply_age_restritions_to(images)
+    images.where('minimum_age >= ?', @user.age)
+  end
+
+  def order_by(sorting)
+    images.order(sorting)
+  end
+
+  def paginate(page)
+    images.limit(PER_PAGE).offset(page.to_i.abs * PER_PAGE)
+  end
+
+  def decorate(images)
     images.map { |image| ImageDecorator.new(image) }
   end
 end
